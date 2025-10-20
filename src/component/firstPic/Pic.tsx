087c23f573f243b32f437fe12a592edf7e4f4020
Florian-Mas <florian.massart@epitech.eu>
2025-10-20 21:14:03 +0200

    fixed the element en the firstPic

--- src/component/firstPic/Pic.tsx
+++ src/component/firstPic/Pic.tsx
@@ -3,10 +3,12 @@ import "./Pic.css";
 function Pic() {
   return (
     <>
-        <div id="pic"/>
+      <div id="pic" />
+      <div className="TextFirstPic">
         <div className="BaseText">Florian MASSART</div>
-        <div className="BaseText description">Étudiant en informatique Web / Logiciel / Cybersécurité</div>
-
+        <div className="BaseText description">Étudiant en informatique Web / Logiciel / Cybersécurité
+        </div>
+      </div>
     </>
   );
 }
