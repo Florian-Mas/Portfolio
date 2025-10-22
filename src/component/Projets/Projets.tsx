9608ea750097852c8637897b6b0d593f657353d9
Florian-Mas <florian.massart@epitech.eu>
2025-10-22 23:41:47 +0200

    projets section design adjusted

--- src/component/Projets/Projets.tsx
+++ src/component/Projets/Projets.tsx
@@ -1,10 +1,10 @@
 import "./Projets.css";
 
 const projetsName = {
-  JobBoard: "JobBoard",
-  PortfolioFormatCV: " Portfolio Forma CV",
-  Hangman: "Hangman",
-  EbaucheJeu3D: "Ebauche jeu 3D",
+  JobBoard: <strong>JobBoard</strong>,
+  PortfolioFormatCV: <strong>Portfolio Format CV</strong>,
+  Hangman: <strong>Hangman</strong>,
+  EbaucheJeu3D: <strong>Ebauche jeu 3D</strong>,
 };
 
 function Projets() {
@@ -12,13 +12,13 @@ function Projets() {
 
   const projetsDescription = {
     JobBoard:
-      "Travail de Groupe pour la création d’un site d’annonce pour l’emploi avec la possibilité de s’inscrire, de se connecter, de postuler et de créer des annonces. Celui qui a posté l'annonce peut ensuite voir quel personne à effectivement postulé avec les éléments pour le recontacter",
+      "Travail de groupe pour la création d’un site d’annonces pour l’emploi avec la possibilité de s’inscrire, de se connecter, de postuler et de créer des annonces. L'annonceur peut ensuite voir quelle personne a postulé avec les éléments du formulaire pour le recontacter.",
     PortfolioFormatCV:
-      "Réalisation de mon portfolio en format CV, cela a été fait sur une seul page ou les éléments s'affiche en fonction de la catégorie choisi",
+      "Réalisation de mon portfolio en format CV. Cela a été réalisé sur une seule page où les éléments s'affichent en fonction de la catégorie choisie.",
     Hangman:
-      "Réalisation d’un jeu du pendu en python avec un choix de mot aléatoire choisi dans une liste personnalisée et enregistrant le meilleur score pour chacun des mots. il est possible de'ajouter des éléments dans la liste afin que le programme essaye de nous les faire découvir",
+      "Réalisation d’un jeu du pendu en python avec un choix de mot aléatoire sélectionné dans une liste personnalisée tout en enregistrant le meilleur score pour chacun des mots. Il est possible d'ajouter des éléments dans la liste afin que le programme essaie de les faire découvir au joueur.",
     EbaucheJeu3D:
-      "Elaboration d’un système de déplacement, de mouvement de caméra, de raycast et de gravité sur Unity lors d’un projet personnel en utilisant uniquement des scripts permettant par la suite d'importer ces scripts sur tout objets permettant de faire l'action",
+      "Elaboration d’un système de déplacement, de mouvement de caméra, de raycast et de gravité sur Unity. Ce projet personnel utilise uniquement des scripts permettant par la suite d'importer ces scripts sur tout élément permettant de faire une action.",
   };
   const projetsVideo = {
     JobBoard: "aaaaa",
@@ -28,10 +28,10 @@ function Projets() {
   };
 
   const projetsLanguage = {
-    JobBoard: "HTML / CSS / JavaScript / Bootstrap / PHP / MySQL",
-    PortfolioFormatCV: "HTML / CSS / JavaScript / Bootstrap",
-    Hangman: "Python",
-    EbaucheJeu3D: "C#",
+    JobBoard: <i>HTML / CSS / JavaScript / Bootstrap / PHP / MySQL</i>,
+    PortfolioFormatCV: <i>HTML / CSS / JavaScript / Bootstrap</i>,
+    Hangman: <i>Python</i>,
+    EbaucheJeu3D: <i>C#</i>,
   };
 
   return (
