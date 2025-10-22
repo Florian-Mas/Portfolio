d97e13c94f80bda92d91b9d4300e8a514d82737f
Florian-Mas <florian.massart@epitech.eu>
2025-10-23 01:01:42 +0200

    navBar working on computer

--- src/component/navBar/navBar.tsx
+++ src/component/navBar/navBar.tsx
@@ -1,13 +1,37 @@
 import "./navBar.css";
 
-function Nav_Bar(){
-    return(
-        <nav id="navBar">
-            A PROPOS / COMPETENCES / FORMATION / EXPERIENCES / PROJETS / CONTACT
-        </nav>)
-    
-    
-    
+function Nav_Bar() {
+  return (
+    <>
+      <nav id="navBar">
+        <div id="listNavBar">
+          <a href="#A_Propos" id="navAP" className="navFixed" role="button">
+            A PROPOS
+          </a>
+
+          <a href="#Competences" id="navCP" className="navFixed">
+            COMPETENCES
+          </a>
+
+          <a href="#Formation" id="navFM" className="navFixed">
+            FORMATION
+          </a>
+
+          <a href="#Experiences" id="navEX" className="navFixed">
+            EXPERIENCES
+          </a>
+
+          <a href="#Projets" id="navPJ" className="navFixed">
+            PROJETS
+          </a>
+
+          <a href="#Contact" id="navC" className="navFixed">
+            CONTACT
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Nav_Bar;
